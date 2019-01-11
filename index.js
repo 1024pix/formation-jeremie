a58d3c3020b73f33594be6b962100eafc60d4180
Jeremie <jeremie.jade@pix.fr>
2019-01-11 17:16:02 +0100

    Fix Scalingo deployment

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const Hapi = require('hapi');
 
 // Create a server with a host and port
 const server = Hapi.server({
-    host: 'localhost',
-    port: 8000
+    port: process.env.PORT||3000
 });
 
 // Add the route
