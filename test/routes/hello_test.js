5f9e5f996e552a7fe4f90b51d35d706864690f67
Jeremie <jeremie.jade@pix.fr>
2019-01-25 14:50:54 +0100

    Setup Hapi testing environnement

---
+++ test/routes/hello_test.js
@@ -0,0 +1,16 @@
+const server = require('../../lib/server');
+const {expect} = require('../test-helper');
+
+describe('route GET /hello', () => {
+
+  it('should return hello world', async () => {
+    // given
+
+    // when
+    const response = await server.inject('/hello');
+
+    // then
+    expect(response.statusCode).to.equal(200);
+    expect(response.result).to.equal('hello world');
+  });
+});
